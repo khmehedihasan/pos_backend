d78f31f58ad2dc4d4cffe5843d8d1d200dde6e11
khmehedihasan <mehedihasan47341@gmail.com>
2022-02-01 15:53:32 +0600

    necessary module added.

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-console.log('pos_backend');
+require('dotenv').config();
+const express = require('express');
+const app = express();
+
+
+
+
+app.get('/', async (req,res,next)=>{
+
+   try{
+       
+    res.send('hello');
+
+   }catch(error){
+
+    next(error);
+
+   }
+})
+
+
+
+
+
+
+app.use(require('./src/Middlewares/errorHandler'))
+
+
+app.listen(process.env.PORT,()=>{
+    console.log(`App is running on port ${process.env.PORT}...`);
+});
