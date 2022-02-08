dfcf87ed691686e55a7b2bfbff6f3dd0613e57a4
khmehedihasan <mehedihasan47341@gmail.com>
2022-02-08 19:12:44 +0600

    update

--- src/Routers/category.js
+++ src/Routers/category.js
@@ -2,14 +2,15 @@ const express = require('express');
 const router = express.Router();
 const category = require('../Controllers/category');
 const uploadPhoto = require('../Middlewares/uploadPhoto');
+const validObjectId = require('../Middlewares/validObjectId');
 
 router.get('/', category.allCategory);
 
-router.get('/:id', category.singleCategory);
+router.get('/:id',validObjectId ,category.singleCategory);
 
-router.post('/',uploadPhoto.single('photo') ,category.addCategory)
+router.post('/', uploadPhoto.single('photo') ,category.addCategory)
 
-router.put('/', category.updateCategory);
+router.put('/:id', validObjectId, uploadPhoto.single('photo') ,category.updateCategory);
 
 router.delete('/', category.deleteCategory);
 
