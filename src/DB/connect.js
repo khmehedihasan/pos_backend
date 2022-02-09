4e12f474719d35f04a747dcfcbc6d8bb57f8b960
khmehedihasan <mehedihasan47341@gmail.com>
2022-02-09 18:52:29 +0600

    update

--- src/DB/connect.js
+++ src/DB/connect.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.DB).then(()=>{
+mongoose.connect(process.env.DB_LOCAL).then(()=>{
 
     console.log('Database connect successfully.');
 
