1300d8b9c3650e34c3675b25f19fd1faed761abb
khmehedihasan <mehedihasan47341@gmail.com>
2022-02-10 17:49:13 +0600

    updte

--- src/Routers/subCategory.js
+++ src/Routers/subCategory.js
@@ -2,16 +2,18 @@ const express = require('express');
 const router = express.Router();
 const subCategory = require('../Controllers/subCategory');
 const uploadPhoto = require('../Middlewares/uploadPhoto');
+const validObjectId = require('../Middlewares/validObjectId');
+
 
 
 router.get('/', subCategory.allSubCategory);
 
-router.get('/:id', subCategory.singleSubCategory);
+router.get('/:id', validObjectId, subCategory.singleSubCategory);
 
 router.post('/', uploadPhoto.single('photo'), subCategory.addSubCategory)
 
-router.put('/', subCategory.updateSubCategory);
+router.put('/', validObjectId, uploadPhoto.single('photo'), subCategory.updateSubCategory);
 
-router.delete('/', subCategory.deleteSubCategory);
+router.delete('/:id', validObjectId, subCategory.deleteSubCategory);
 
 module.exports = router;
