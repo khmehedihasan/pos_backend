62723d2c19415fa051004214ee2188cafd1e619a
khmehedihasan <mehedihasan47341@gmail.com>
2022-02-10 17:01:31 +0600

    Code for sub category add  done.

--- src/Controllers/subCategory.js
+++ src/Controllers/subCategory.js
@@ -1,11 +1,53 @@
 const SubCategory = require('../Models/SubCategory');
+const Category = require('../Models/Category');
 
 
 //-------------------------------------------------------add SubCategory------------------------------------------------
 
 exports.addSubCategory = async (req,res,next)=>{
     try{
-        res.send("ok");
+
+
+        if(req.file == undefined){
+
+            const data = await SubCategory({
+                name:req.body.name,
+                description:req.body.description,
+                category: req.body.categoryId
+            });
+        
+
+    
+            const d = await data.save();
+    
+            if(d != {}){
+                const dc = await Category.findByIdAndUpdate(req.body.categoryId,{$push:{subCategorys:d._id}});
+                res.send({status:true,message:"Sub Category added successfully."});
+            }else{
+                res.send({status:true,message:"Faild to added Sub Category."});
+            }
+
+        }else{
+            const photo = req.file.filename;
+            const image = process.env.PUBLIC_LINK+req.file.filename;
+            const data = await SubCategory({
+                name:req.body.name,
+                description:req.body.description,
+                img:image,
+                photo:photo,
+                category: req.body.categoryId
+            });
+    
+            const d = await data.save();
+    
+            if(d != {}){
+                const dc = await Category.findByIdAndUpdate(req.body.categoryId,{$push:{subCategorys:d._id}});
+                res.send({status:true,message:"Sub Category added successfully."});
+            }else{
+                res.send({status:true,message:"Faild to added Sub Category."});
+            }
+        }
+
     }catch(error){
         next(error);
     }
