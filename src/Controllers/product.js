3b3e5d763e5af0b5125c6e7f09ae9729247b41f1
khmehedihasan <mehedihasan47341@gmail.com>
2022-02-13 21:05:58 +0600

    Code for add product done

--- src/Controllers/product.js
+++ src/Controllers/product.js
@@ -1,11 +1,58 @@
 const Product = require('../Models/Product');
+const SubCategory = require('../Models/SubCategory');
 
 
 //-------------------------------------------------------add Product------------------------------------------------
 
 exports.addProduct = async (req,res,next)=>{
     try{
-        res.send("ok");
+
+        if(req.file == undefined){
+
+            const data = await Product({
+                name:req.body.name,
+                description:req.body.description,
+                purchasePrice:req.body.purchasePrice,
+                salePrice:req.body.salePrice,
+                subCategory: req.body.subCategoryId
+            });
+        
+
+    
+            const d = await data.save();
+    
+            if(d != {}){
+                const dc = await SubCategory.findByIdAndUpdate(req.body.subCategoryId,{$push:{products:d._id}});
+                res.send({status:true,message:"Product added successfully."});
+            }else{
+                res.send({status:true,message:"Faild to added product."});
+            }
+
+        }else{
+            const photo = req.file.filename;
+            const image = process.env.PUBLIC_LINK+req.file.filename;
+            const data = await Product({
+                name:req.body.name,
+                description:req.body.description,
+                purchasePrice:req.body.purchasePrice,
+                salePrice:req.body.salePrice,
+                subCategory: req.body.subCategoryId,
+                img:image,
+                photo:photo
+                
+            });
+    
+            const d = await data.save();
+    
+            if(d != {}){
+                const dc = await SubCategory.findByIdAndUpdate(req.body.subCategoryId,{$push:{products:d._id}});
+                res.send({status:true,message:"Product added successfully."});
+            }else{
+                res.send({status:true,message:"Faild to added product."});
+            }
+        }
+
+        
     }catch(error){
         next(error);
     }
