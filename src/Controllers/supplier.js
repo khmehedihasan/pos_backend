3b64cf5fb8db633ed7bf676515809caaaebace1a
khmehedihasan <mehedihasan47341@gmail.com>
2022-02-15 06:59:32 +0600

    Code for add supplier done

--- src/Controllers/supplier.js
+++ src/Controllers/supplier.js
@@ -5,7 +5,47 @@ const Supplier = require('../Models/Supplier');
 
 exports.addSupplier = async (req,res,next)=>{
     try{
-        res.send('ok')
+        
+        if(req.file == undefined){
+
+            const data = await Supplier({
+                name:req.body.name,
+                email:req.body.description,
+                phone:req.body.description,
+                address:req.body.description
+            });
+        
+
+    
+            const d = await data.save();
+    
+            if(d != {}){
+                res.send({status:true,message:"Supplier added successfully."});
+            }else{
+                res.send({status:true,message:"Faild to added Supplier."});
+            }
+
+        }else{
+            const photo = req.file.filename;
+            const image = process.env.PUBLIC_LINK+req.file.filename;
+            const data = await Supplier({
+                name:req.body.name,
+                email:req.body.description,
+                phone:req.body.description,
+                address:req.body.description,
+                img:image,
+                photo:photo,
+            });
+    
+            const d = await data.save();
+    
+            if(d != {}){
+                res.send({status:true,message:"Supplier added successfully."});
+            }else{
+                res.send({status:true,message:"Faild to added Supplier."});
+            }
+        }
+
     }catch(error){
         next(error);
     }
