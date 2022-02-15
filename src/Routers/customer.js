70b4534aa8fb9f8d690ab16212118b5955294d1d
khmehedihasan <mehedihasan47341@gmail.com>
2022-02-15 11:43:38 +0600

    update

--- src/Routers/customer.js
+++ src/Routers/customer.js
@@ -1,15 +1,17 @@
 const express = require('express');
 const router = express.Router();
 const customer = require('../Controllers/customer');
+const validObjectId = require('../Middlewares/validObjectId');
+const uploadPhoto = require('../Middlewares/uploadPhoto');
 
 router.get('/', customer.allCustomer);
 
-router.get('/:id', customer.singleCustomer);
+router.get('/:id', validObjectId, customer.singleCustomer);
 
-router.post('/', customer.addCustomer)
+router.post('/:id', validObjectId, uploadPhoto.single('photo'), customer.addCustomer)
 
-router.put('/', customer.updateCustomer);
+router.put('/:id', validObjectId, uploadPhoto.single('photo'), customer.updateCustomer);
 
-router.delete('/', customer.deleteCustomer);
+router.delete('/:id', validObjectId, customer.deleteCustomer);
 
 module.exports = router;
