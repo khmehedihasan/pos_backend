72f8ca846d8c89b78367a744fee97a0ee3347902
khmehedihasan <mehedihasan47341@gmail.com>
2022-02-16 19:08:10 +0600

    Code for purchase product done

--- src/Controllers/purchase.js
+++ src/Controllers/purchase.js
@@ -1,11 +1,52 @@
 const Purchase = require('../Models/Purchase');
+const Product = require('../Models/Product');
+const Supplier = require('../Models/Supplier');
 
 
 //-------------------------------------------------------add Purchase------------------------------------------------
 
 exports.addPurchase = async (req,res,next)=>{
     try{
-        res.send("ok");
+
+        const data = await Purchase({
+            product:req.body.productId,
+            supplier:req.body.supplierId,
+            payable: req.body.payable,
+            payed: req.body.payed,
+            due: req.body.payable - req.body.payed,
+            quantity:req.body.quantity,
+        });
+    
+
+        const d = await data.save();
+
+        if(d != {}){
+
+            const dt = await Product.findById(req.body.productId);
+            if(dt != null){
+
+                const purchaseQuantity = (parseInt(req.body.quantity) + parseInt(dt.purchaseQuantity));
+                const inStock = (parseInt(req.body.quantity) + parseInt(dt.inStock));
+
+                const dc = await Product.findByIdAndUpdate(req.body.productId,{$set:{purchaseQuantity,inStock},$push:{purchases:d._id}});
+            }
+            
+
+            const dtt = await Supplier.findById(req.body.supplierId);
+            if(dtt != null){
+
+                const payable = (parseInt(req.body.payable) + parseInt(dtt.payable));
+                const payed = (parseInt(req.body.payed) + parseInt(dtt.payed));
+                const due = (parseInt(d.due) + parseInt(dtt.due));
+
+                const dc = await Supplier.findByIdAndUpdate(req.body.supplierId,{$set:{payable,payed,due},$push:{purchases:d._id}});
+            }
+
+            res.send({status:true,message:"Product purchase successfully."});
+        }else{
+            res.send({status:true,message:"Product failed to purchase."});
+        }
+
     }catch(error){
         next(error);
     }
