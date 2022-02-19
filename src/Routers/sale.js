7fa2cc0eb1a9e34e4d0cbdc1a4e7c5a4bd161fcf
khmehedihasan <mehedihasan47341@gmail.com>
2022-02-19 20:58:22 +0600

    update

--- src/Routers/sale.js
+++ src/Routers/sale.js
@@ -1,15 +1,17 @@
 const express = require('express');
 const router = express.Router();
 const sale = require('../Controllers/sale');
+const validObjectId = require('../Middlewares/validObjectId');
+
 
 router.get('/', sale.allSale);
 
-router.get('/:id', sale.singleSale);
+router.get('/:id', validObjectId, sale.singleSale);
 
 router.post('/', sale.addSale)
 
-router.put('/', sale.updateSale);
+router.put('/:id', validObjectId, sale.updateSale);
 
-router.delete('/', sale.deleteSale);
+router.delete('/:id', validObjectId, sale.deleteSale);
 
 module.exports = router;
