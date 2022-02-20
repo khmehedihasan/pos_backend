208fc401631b90021b50f1013cbe23651ac8595d
khmehedihasan <mehedihasan47341@gmail.com>
2022-02-21 00:47:25 +0600

    update

--- src/Controllers/purchase.js
+++ src/Controllers/purchase.js
@@ -9,13 +9,15 @@ exports.addPurchase = async (req,res,next)=>{
     try{
 
         const dtt = await Supplier.findById(req.body.supplierId);
+        const dt = await Product.findById(req.body.productId);
+
 
         const data = await Purchase({
             product:req.body.productId,
             supplier:req.body.supplierId,
-            payable: req.body.payable,
+            payable: parseInt(dt.salePrice) * parseInt(req.body.quantity),
             payed: req.body.payed,
-            due: req.body.payable - req.body.payed,
+            due: (dt.salePrice * parseInt(req.body.quantity)) - req.body.payed,
             quantity:req.body.quantity,
             supplierName:dtt.name,
             supplierEmail:dtt.email,
@@ -28,7 +30,6 @@ exports.addPurchase = async (req,res,next)=>{
 
         if(d != {}){
 
-            const dt = await Product.findById(req.body.productId);
             if(dt != null){
 
                 const purchaseQuantity = (parseInt(req.body.quantity) + parseInt(dt.purchaseQuantity));
@@ -40,7 +41,7 @@ exports.addPurchase = async (req,res,next)=>{
 
             if(dtt != null){
 
-                const payable = (parseInt(req.body.payable) + parseInt(dtt.payable));
+                const payable = (parseInt(d.payable) + parseInt(dtt.payable));
                 const payed = (parseInt(req.body.payed) + parseInt(dtt.payed));
                 const due = (parseInt(d.due) + parseInt(dtt.due));
 
