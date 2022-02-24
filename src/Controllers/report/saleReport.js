759089b1723b9299152490ea8919847fe8a94ae3
khmehedihasan <mehedihasan47341@gmail.com>
2022-02-24 09:59:04 +0600

    update

---
+++ src/Controllers/report/saleReport.js
@@ -0,0 +1,68 @@
+const Sale = require('../../Models/Sale');
+
+
+
+//--------------------------------------get total received money----------------------------------
+
+exports.totalReceived = async (req,res,next) =>{
+    try{
+        const data = await Sale.aggregate([
+            {
+              "$group": {
+                "_id": null,
+                "total": {
+                  "$sum": "$received"
+                }
+              }
+            }
+          ]);
+
+        res.send(data)
+    }catch(error){
+        next(error)
+    }
+}
+
+
+
+//--------------------------------------get total sales due----------------------------------
+
+exports.totalDue = async (req,res,next) =>{
+    try{
+        const data = await Sale.aggregate([
+            {
+              "$group": {
+                "_id": null,
+                "total": {
+                  "$sum": "$due"
+                }
+              }
+            }
+          ]);
+
+        res.send(data)
+    }catch(error){
+        next(error)
+    }
+}
+
+//--------------------------------------get total sales porduct quantity----------------------------------
+
+exports.totalPorduct = async (req,res,next) =>{
+    try{
+        const data = await Sale.aggregate([
+            {
+              "$group": {
+                "_id": null,
+                "total": {
+                  "$sum": "$quantity"
+                }
+              }
+            }
+          ]);
+
+        res.send(data)
+    }catch(error){
+        next(error)
+    }
+}
