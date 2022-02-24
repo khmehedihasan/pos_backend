2975907bdc5e147c67846e0142385683b4ccc817
khmehedihasan <mehedihasan47341@gmail.com>
2022-02-24 09:58:39 +0600

    update

--- src/Routers/report.js
+++ src/Routers/report.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const router = express.Router();
-const report = require('../Controllers/report')
+const report = require('../Controllers/report/saleReport')
 
 router.get('/totalReceived',report.totalReceived);
 router.get('/totalDue',report.totalDue);
+router.get('/totalPorduct',report.totalPorduct);
 
 module.exports = router;
