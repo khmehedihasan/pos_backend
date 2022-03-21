394fb79bc25f4f03334e01bb36c7972300f9934d
khmehedihasan <mehedihasan47341@gmail.com>
2022-03-22 01:00:46 +0600

    update

--- src/Middlewares/allowHeaders.js
+++ src/Middlewares/allowHeaders.js
@@ -1,7 +1,8 @@
 const allowHeaders = async (req,res,next)=>{
     try{
-        res.header('Access-Control-Allow-Origin', req.headers.origin);
+        res.header('Access-Control-Allow-Origin', "*");
         res.header('Access-Control-Allow-Credentials', 'true');
+        res.header("Access-Control-Allow-Methods", "*"); 
         next();
     }catch(error){
         next(error);
